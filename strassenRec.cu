b804d0381b4e57f913b149a5f1e0bda53e92dcf4
vishal340 <45174667+vishal340@users.noreply.github.com>
2019-12-02 07:29:20 +0530

    Update strassenRec.cu

--- strassenRec.cu
+++ strassenRec.cu
@@ -31,7 +31,7 @@ __global__ void  multiply(float* A,float* B,float* C,int jump,int jump1,int jump
 
 //non_square matrix multiplication
 
-__global__ void kernel1(float* A,int jump1,int n1,float* B,int jump2,int n2,float* C,int jump3,float* D,int jump4,                            int m1,int m2,int m3)
+__global__ void kernel1(float* A,int jump1,int n1,float* B,int jump2,int n2,float* C,int jump3,float* D,int jump4,int m1,int m2,int m3)
 {
 	int col=blockIdx.x * blockDim.x+threadIdx.x;
 	int row=blockIdx.y * blockDim.y+threadIdx.y;
@@ -40,7 +40,7 @@ __global__ void kernel1(float* A,int jump1,int n1,float* B,int jump2,int n2,floa
 	if(col<m3 && row<m2)
 		D[col+jump4*row]=B[col+jump2*row]+B[n2+col+jump2*row];
 }
-__global__ void kernel7(float* A,int jump1,int n1,float* B,int jump2,int n2,float* C,int jump3,float* D,int jump4,                            int m1,int m2,int m3)
+__global__ void kernel7(float* A,int jump1,int n1,float* B,int jump2,int n2,float* C,int jump3,float* D,int jump4,int m1,int m2,int m3)
 {
 	int col=blockIdx.x * blockDim.x+threadIdx.x;
 	int row=blockIdx.y * blockDim.y+threadIdx.y;
@@ -84,7 +84,7 @@ __global__ void kernel3(float* A,int jump,int n,float* B,int jump1,float* C,int 
 		B[col+jump1*row]=C[col+jump2*row]+C[col+jump2*row+n1];
 }
 
-__global__ void kernel4(float* A,int jump,int n1,float* B,int jump1,float* C,int jump2,float* D,int jump3,int n2,                             int m1,int m2)
+__global__ void kernel4(float* A,int jump,int n1,float* B,int jump1,float* C,int jump2,float* D,int jump3,int n2,int m1,int m2)
 {
 	int col=blockIdx.x * blockDim.x+threadIdx.x,row;
 	if(2*blockIdx.y<gridDim.y)
@@ -113,7 +113,7 @@ __global__ void kernel5(float* A,int jump,int n1,float* B,int jump1)
 	A[point+n1]+=B[point1];
 }
 
-__global__ void kernel6(float* A,int jump,float* B,int jump1,int n1,float* C,int jump2,float* D,int jump3,int n2,                             int m1,int m2,int m3)
+__global__ void kernel6(float* A,int jump,float* B,int jump1,int n1,float* C,int jump2,float* D,int jump3,int n2,int m1,int m2,int m3)
 {
 	int col=blockIdx.x*blockDim.x+threadIdx.x;
 	if(col<m2 && 3*blockIdx.y<gridDim.y)
@@ -138,7 +138,7 @@ __global__ void kernel6(float* A,int jump,float* B,int jump1,int n1,float* C,int
 
 void strassen(float* A,int jump,float* B,int jump1,float* C,int jump2,int n1,int n2,int n3,float* temp1,float* temp2,int n,int n_)
 {
-  if(n1<=threshold || n2<=threshold || n3<=threshold)
+  if(n1<=threshold || n3<=threshold)
   {
     multiply <<<dim3(n3/xthread,n1/xthread),dim3(xthread,xthread)>>> (A,B,C,jump,jump1,jump2,n2/xthread);
   }
@@ -147,24 +147,24 @@ void strassen(float* A,int jump,float* B,int jump1,float* C,int jump2,int n1,int
     n1/=2;n2/=2;n3/=2;
     n/=2;n_/=2;
     //M1
-    kernel1 <<<dim3(n_/xthread,n/xthread),dim3(xthread,xthread)>>> (A,jump,jump*n1+n2,B,jump1,jump1*n2+n3,temp1,n2,       temp2,n3,n1,n2,n3);   //temp1=A11+A22 //temp2=B11+B22
+    kernel1 <<<dim3(n_/xthread,n/xthread),dim3(xthread,xthread)>>> (A,jump,jump*n1+n2,B,jump1,jump1*n2+n3,temp1,n2,temp2,n3,n1,n2,n3);   //temp1=A11+A22 //temp2=B11+B22
     strassen(temp1,n2,temp2,n3,C,jump2,n1,n2,n3,temp1+n1*n2,temp2+n2*n3,n,n_);   //C11=temp1*temp2
     //M6
-    kernel7 <<<dim3(n_/xthread,n/xthread),dim3(xthread,xthread)>>> (A+jump*n1,jump,-jump*n1,B,jump1,n3,temp1,n2,         temp2,n3,n1,n2,n3);   //temp1=A21-A11//temp2=B11+B12
+    kernel7 <<<dim3(n_/xthread,n/xthread),dim3(xthread,xthread)>>> (A+jump*n1,jump,-jump*n1,B,jump1,n3,temp1,n2,temp2,n3,n1,n2,n3);   //temp1=A21-A11//temp2=B11+B12
     strassen(temp1,n2,temp2,n3,C+jump2*n1+n3,jump2,n1,n2,n3,temp1+n1*n2,temp2+n2*n3,n,n_);   //C22=temp1*temp2
     //M7
-    kernel7 <<<dim3(n_/xthread,n/xthread),dim3(xthread,xthread)>>> (A+n2,jump,jump*n1,B+jump1*n2,jump1,n3,temp1,n2,      temp2,n3,n1,n2,n3);//temp1=A12-A22 //temp2=B21+B22
+    kernel7 <<<dim3(n_/xthread,n/xthread),dim3(xthread,xthread)>>> (A+n2,jump,jump*n1,B+jump1*n2,jump1,n3,temp1,n2,temp2,n3,n1,n2,n3);//temp1=A12-A22 //temp2=B21+B22
     strassen(temp1,n2,temp2,n3,C+n3,jump2,n1,n2,n3,temp1+n1*n2,temp2+n2*n3,n,n_);  //C12+=temp1*temp2
-    kernel2 <<<dim3(n_/xthread,(3*n1)/xthread),dim3(xthread,xthread)>>> (C,jump2,n3,jump2*n1+n3,temp1,n2,A+jump*n1,       jump,n2,n2,n3);  //C22+=C11//C11+=C12 //temp1=A21+A22 //C12=0
+    kernel2 <<<dim3(n_/xthread,(3*n1)/xthread),dim3(xthread,xthread)>>> (C,jump2,n3,jump2*n1+n3,temp1,n2,A+jump*n1,jump,n2,n2,n3);  //C22+=C11//C11+=C12 //temp1=A21+A22 //C12=0
     //M2
     strassen(temp1,n2,B,jump1,C+jump2*n1,jump2,n1,n2,n3,temp1+n1*n2,temp2+n2*n3,n,n_);  //C21=temp1*B11
     kernel3 <<<dim3(n_/xthread,n1/xthread),dim3(xthread,xthread)>>> (C+jump2*n1,jump2,n3,temp1,n2,A,jump,n2,n2,n3);       //C22-=C21 //temp1=A11+A12
     //M5
     strassen(temp1,n2,B+jump1*n2+n3,jump1,C+n3,jump2,n1,n2,n3,temp1+n1*n2,temp2+n2*n3,n,n_);  //C12=temp1*B22
-    kernel6<<<dim3(n_/xthread,(3*n)/xthread),dim3(xthread,xthread)>>>(temp1,n2,C,jump2,n3,temp2,n3,B+n3,jump1,            jump1*n2,n1,n2,n3); //C11-=C12 //temp2=B12-B22 //temp1=0
+    kernel6<<<dim3(n_/xthread,(3*n)/xthread),dim3(xthread,xthread)>>>(temp1,n2,C,jump2,n3,temp2,n3,B+n3,jump1,jump1*n2,n1,n2,n3); //C11-=C12 //temp2=B12-B22 //temp1=0
     //M3
     strassen(A,jump,temp2,n3,temp1,n3,n1,n2,n3,temp1+n1*n3,temp2+n2*n3,n,n_);  //temp1=A11*temp2
-    kernel4 <<<dim3(n3/xthread,(2*n)/xthread),dim3(xthread,xthread)>>> (C+n3,jump2,jump2*n1,temp1,n2,temp2,n3,B,jump1,    jump1*n2,n1,n2);  //C12=C12+temp1//C22=C22+temp1 //temp2=B21-B11 //temp1=0
+    kernel4 <<<dim3(n3/xthread,(2*n)/xthread),dim3(xthread,xthread)>>> (C+n3,jump2,jump2*n1,temp1,n2,temp2,n3,B,jump1,jump1*n2,n1,n2);  //C12=C12+temp1//C22=C22+temp1 //temp2=B21-B11 //temp1=0
     //M4
     strassen(A+jump*n1+n2,jump,temp2,n3,temp1,n3,n1,n2,n3,temp1+n1*n3,temp2+n2*n3,n,n_);  //temp1=A22*temp2
     kernel5 <<<dim3(n3/xthread,n1/xthread),dim3(xthread,xthread)>>> (C,jump2,jump2*n1,temp1,n2);                          //C21=C21+temp1//C11=C11+temp1
@@ -261,19 +261,19 @@ void strassen_sq(float* A,int jump,float* B,int jump1,float* C,int jump2,int n,f
   {
 	n/=2;
 	block_len/=2;
-     kernel1_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(A,jump,jump*n+n,B,jump1,jump1*n+n,temp1,temp2,n);
+     	kernel1_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(A,jump,jump*n+n,B,jump1,jump1*n+n,temp1,temp2,n);
 	strassen_sq(temp1,n,temp2,n,C,jump2,n,temp1+n*n,temp2+n*n,block_len);
-       kernel7_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(A+jump*n,jump,-jump*n,B,jump1,n,temp1,temp2,n);
+        kernel7_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(A+jump*n,jump,-jump*n,B,jump1,n,temp1,temp2,n);
 	strassen_sq(temp1,n,temp2,n,C+jump2*n+n,jump2,n,temp1+n*n,temp2+n*n,block_len);
-     kernel7_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(A+n,jump,jump*n,B+jump1*n,jump1,n,temp1,temp2,n);
+     	kernel7_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(A+n,jump,jump*n,B+jump1*n,jump1,n,temp1,temp2,n);
 	strassen_sq(temp1,n,temp2,n,C+n,jump2,n,temp1+n*n,temp2+n*n,block_len);
-       kernel2_sq<<<dim3(block_len,3*block_len),dim3(xthread,xthread)>>>(C,jump2,n,jump2*n+n,temp1,n,A+jump*n,jump,n);
+        kernel2_sq<<<dim3(block_len,3*block_len),dim3(xthread,xthread)>>>(C,jump2,n,jump2*n+n,temp1,n,A+jump*n,jump,n);
 	strassen_sq(temp1,n,B,jump1,C+jump2*n,jump2,n,temp1+n*n,temp2+n*n,block_len);
 	kernel3_sq<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(C+jump2*n,jump2,n,temp1,n,A,jump,n);
 	strassen_sq(temp1,n,B+jump1*n+n,jump1,C+n,jump2,n,temp1+n*n,temp2+n*n,block_len);
-       kernel6_sq<<<dim3(block_len,3*block_len),dim3(xthread,xthread)>>>(temp1,n,C,jump2,n,temp2,n,B+n,jump1,jump1*n);
+        kernel6_sq<<<dim3(block_len,3*block_len),dim3(xthread,xthread)>>>(temp1,n,C,jump2,n,temp2,n,B+n,jump1,jump1*n);
 	strassen_sq(A,jump,temp2,n,temp1,n,n,temp1+n*n,temp2+n*n,block_len);
- kernel4_sq<<<dim3(block_len,2*block_len),dim3(xthread,xthread)>>>(C+n,jump2,jump2*n,temp1,n,temp2,n,B,jump1,jump1*n);
+ 	kernel4_sq<<<dim3(block_len,2*block_len),dim3(xthread,xthread)>>>(C+n,jump2,jump2*n,temp1,n,temp2,n,B,jump1,jump1*n);
 	strassen_sq(A+jump*n+n,jump,temp2,n,temp1,n,n,temp1+n*n,temp2+n*n,block_len);
 	kernel5<<<dim3(block_len,block_len),dim3(xthread,xthread)>>>(C,jump2,jump2*n,temp1,n);
   }
@@ -282,22 +282,24 @@ void strassen_sq(float* A,int jump,float* B,int jump1,float* C,int jump2,int n,f
 
 int nearest_ideal(int &n,int &temp)
 {
-  temp=0;
+  int temp1=(xthread-n%xthread)%xthread;
   int pow=1;
-  int m=n;
+  n+=temp1;
+  int m=n/xthread;
   while(m>threshold/xthread)
   {
-	if(m%2==0)
-		pow*=2;
-	else
-	{
-		temp+=pow;
-		m++;
-	}
-	m/=2;
+    if(m%2==1)
+    {
+        temp+=pow;
+        m++;
+    }
+    m/=2;
+    pow*=2;
   }
-  n+=temp;
-  return n/(m*xthread);
+  n+=temp*xthread;
+  temp=temp*xthread;
+  temp+=temp1;
+  return pow;
 }
 
 int main(int argc,char** argv)
@@ -307,9 +309,10 @@ int main(int argc,char** argv)
   std::ofstream out(argv[3]);
   float *A,*B,*C;
   int n1,n2,n3;
-  int temp1,temp2,temp3;
+  int temp1=0,temp2=0,temp3=0;
   in>>n1>>n2;
   in1>>n2>>n3;
+  out<<n1<<'\t'<<n3<<'\n';
   int power=nearest_ideal(n1,temp1);
   power=std::min(power,nearest_ideal(n2,temp2));
   power=std::min(power,nearest_ideal(n3,temp3));
@@ -368,18 +371,18 @@ int main(int argc,char** argv)
   cudaMemcpy (C, d_C, sizeof(float)*n1*n3, cudaMemcpyDeviceToHost);
   printf("Error %s \n",cudaGetErrorString(cudaGetLastError()));
   double time_taken = (end.tv_nsec-start.tv_nsec)+1e+9*(end.tv_sec-start.tv_sec);
-  printf("StrassenRec3 - Time taken: %f\n",time_taken);
+  printf("StrassenRec - Time taken: %f\n",time_taken);
   for(int i=0;i<n1-temp1;i++)
   {
     for(int j=0;j<n3-temp3;j++)
-      out<<C[i*n3+j]<<"\t";
-    out<<"\n";
+      out<<C[i*n3+j]<<'\t';
+    out<<'\n';
   }
+  std::ofstream ofile;
+  ofile.open(argv[4],std::ios_base::app);
+  ofile<<"strassenRec - Time taken (ns): "<<time_taken<<"\n";
+  ofile.close();
 
-	std::ofstream ofile;
-        ofile.open(argv[4],std::ios_base::app);
-        ofile<<"strassenRec1 - Time taken (ns): "<<time_taken<<"\n";
-        ofile.close();
   cudaFree(d_A);
   cudaFree(d_B);
   cudaFree(d_C);
